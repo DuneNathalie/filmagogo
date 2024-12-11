e6b2fd9eb6022faea29d9cac0b779dd053c4695e
dune <nathydune@gmail.com>
2024-12-11 19:40:35 +0100

    link backend and frontend

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home:React.FC = () => {
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/films')
+      .then(response => response.json())
+      .then(data => setFilms(data))
+      .catch(error => console.error('Erreur:', error));
+  }, []);
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Liste de films : </h1>
+      <ul>
+        {films.map(film => (
+          <li key={film.id}>
+            {film.title} - Réalisé par {film.director}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
