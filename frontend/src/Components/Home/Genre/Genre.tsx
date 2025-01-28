20afbe59442c3af97b6c6c79e11d15f1d496281c
dune <nathydune@gmail.com>
2025-01-28 16:35:04 +0100

    update api and component Genre ans gestion swipe genre

--- frontend/src/Components/Home/Genre/Genre.tsx
+++ frontend/src/Components/Home/Genre/Genre.tsx
@@ -2,16 +2,50 @@ import React from 'react';
 
 import Styles from "./Genre.module.scss";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Navigation, A11y } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 interface GenreProps {
-    title: string;
-    image: string;
+    theme?: string;
+    data: Array<{
+        id: number;
+        title?: string;
+        poster_path: string;
+        overview?: string;
+    }>;
 }
 
-const Genre: React.FC<GenreProps> = ({title, image}) => {
+const Genre: React.FC<GenreProps> = ({ theme, data }) => {
+
     return (
         <div className={Styles.container}>
-        <h1>{title}</h1>
-        <p>{image}</p>
+            <h2>{theme}</h2>
+            <div className={Styles.swiper}>
+                <Swiper
+                    modules={[Navigation, A11y]}
+                    spaceBetween={10}
+                    slidesPerView={8}
+                    navigation
+                    style={{
+                        '--swiper-navigation-color': '#fff'
+                    } as React.CSSProperties}
+                >
+                    {data.map((film) => (
+                        <SwiperSlide key={film.id} className={Styles.slide}>
+                            <img
+                                src={`https://image.tmdb.org/t/p/w500${film.poster_path}`}
+                                alt={film.title || "Film"}
+                            />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
         </div>
     );
 };
--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -1,38 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import Film from '../../Components/Home/Film/Film.tsx';
-import Genre from '../../Components/Home/Genre/Genre.tsx';
-import Menu from '../../Components/Menu/Menu.tsx';
+import Genre from "../../Components/Home/Genre/Genre.tsx";
+import Menu from "../../Components/Menu/Menu.tsx";
 
 import Styles from "./Home.module.scss";
 
-import Pocket from "../../assets/pocket.png";
 import User from "../../assets/user.png";
+import Footer from "../../Components/Footer/Footer.tsx";
 
-const Home:React.FC = () => {
- // const [films, setFilms] = useState([]);
 
- /* useEffect(() => {
-    fetch('http://localhost:5000/films')
-      .then(response => response.json())
-      .then(data => setFilms(data))
-      .catch(error => console.error('Erreur:', error));
-  }, []); */
+
+const Home: React.FC = () => {
+const [films, setFilms] = useState<[]>([]);
+
+useEffect(() => {
+  fetch(
+    "https://api.themoviedb.org/3/movie/popular?api_key=a4a7fa2a24f06525feb656146f5305a7"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      setFilms(data.results);
+    })
+    .catch((error) => console.error("Erreur:", error));
+}, []);
+
   return (
     <div className={Styles.container}>
-      <Menu img={User} title='Pour Toto'/>
-      <Film title={'Title'} description={'Imperiali seminecem inpulsu similia formidabat cuiusdam ingravesceret incessens famis sibi addictum quod et subditis ingravesceret miserando seminecem addictum audaciam subditis seminecem vulgi seminecem similia discerpsit similia periculi miserando subditis quisque exitio imaginem imperiali post seminecem discerpsit sibi imaginem famis domum inpulsu lacrimosum ingravesceret documento famis subditis ambitiosam cum inter periculi ut similia inflammavit unius famis rectoremque inflammavit pugnis sordidioris pugnis haec et et sordidioris calcibus ingravesceret recenti ignibus penuria ingravesceret furoris domum quisque imperiali commeatuum furoris audaciam suos formidabat considerans vulgi interitum documento incessens formidabat iudicio laniatu sui quod unius haec lacrimosum exitio famis ignibus inter unius similia inpulsu ingravesceret.'} img={Pocket}/>
-      <Genre title={'Humour'} image={'Image humour'}/>
-      <Genre title={'Drame'} image={'Image drame'}/>
-      <Genre title={'Suspence'} image={'Image Suspence'}/>
-    {/** <ul>
-        {films.map(film => (
-          <li key={film.id}>
-            {film.title} - Réalisé par {film.director}
-          </li>
-        ))}
-      </ul>*/}  
+      <Menu img={User} title="Pour Toto" />
+      <Genre data={films} theme="Populaire"/>
+      <Genre data={films} theme="Comedie"/>
+      <Genre data={films} theme="Thriller"/>
+      <Footer />
     </div>
   );
 };
+
 export default Home;
